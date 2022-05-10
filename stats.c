f46c33bb731f9de1284be2ff56218efd54445e8a
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-05-10 10:38:40 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -6,27 +6,27 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.min = 0;
     s.max = 0;
     int index_count=0,sort_count=0;
-	float sum=0;
+    float sum=0,temp_number=0;
     /*sorting the array*/
-	for (index_count=0;index_count<length;index_count++)
+    for (index_count=0;index_count<length;index_count++)
+    {
+	for(sort_count=0;sort_count<length;sort_count++)
 	{
-		for(sort_count=0;sort_count<length;sort_count++)
-		{
-			if(numberset[index_count]>numberset[sort_count])
-			{
-				temp_number = numberset[index_count];
-				numberset[index_count]= numberset[sort_count];
-				numberset[sort_count]=temp_number;
-			}
-		}
-		sum+=numberset[index_count];
+	   if(numberset[index_count]>numberset[sort_count])
+	    {
+		temp_number = numberset[index_count];
+		numberset[index_count]= numberset[sort_count];
+		numberset[sort_count]=temp_number;
+	    }
 	}
+	sum+=numberset[index_count];
+     }
     /*average*/
-	s.average=(sum/setlength);
+    s.average=(sum/setlength);
     /*minimum*/
-	s.min=numberset[0];
+    s.min=numberset[0];
     /*maximum*/
-	s.max=numberset[setlength-1];
+    s.max=numberset[setlength-1];
     return s;
 }
 
