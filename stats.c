7a8917f48ac0faa4be4c9a8141a2e26a8969a1e2
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-05-10 14:09:01 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,24 +7,28 @@ int ledAlertCallCount = 0;
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats set;
     set.average = 0;
-    set.min = numberset[0];
+    set.min = 0;
     set.max = 0;
     int index_count=0;
     float sum=0.0;
-    for (;index_count<setlength;index_count++)
+    if(setlength!=0)
     {
-        if(numberset[index_count]<set.min)
+        set.min = numberset[0];
+	for (;index_count<setlength;index_count++)
 	{
+	     if(numberset[index_count]<set.min)
+	     {
 		set.min=numberset[index_count];
-	}
-	if(numberset[index_count]>set.max)
-	{
+	     }
+	     if(numberset[index_count]>set.max)
+	     {
 		set.max=numberset[index_count];
-	}    
-	sum+=numberset[index_count];
-     }
-    set.average=(sum/setlength);
-    return set;
+	     }    
+	     sum+=numberset[index_count];
+	}
+	set.average=(sum/setlength);
+   }
+   return set;
 }
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[],struct Stats computedStats)
 {
